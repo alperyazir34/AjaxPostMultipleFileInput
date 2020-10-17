35cbe2a5494c064585e0554a0600eb4f904cde8b
alper yazır <alper.yazir@outlook.com>
2020-10-17 20:27:09 +0300

    project pertama

---
+++ AjaxFileInput/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxFileInput.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase[] files,int? id)//parametre adı files view ID si ile aynı olmak zorunda
+        {
+            for (int i = 0; i < files.Length; i++)
+            {
+                var folder = Server.MapPath("~/Upload");
+                var randomFileName = Path.GetRandomFileName();
+                var FileName = Path.ChangeExtension(randomFileName, ".jpg");
+                var path = Path.Combine(folder, FileName);
+
+                files[i].SaveAs(path);
+            }
+            return Json("");
+        }
+    }
+}
